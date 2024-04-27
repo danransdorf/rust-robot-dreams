a59aaedef049339643edafe2570e3c1ede57e498
MonteBro <czech.illusion@gmail.com>
2024-04-27 17:46:37 +0200

    lesson 2

---
+++ lesson2/src/main.rs
@@ -0,0 +1,24 @@
+use slug::slugify;
+use std::env;
+use std::io;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read input.");
+
+    if args[1].as_str() == "lowercase" {
+        println!("{}", input.to_lowercase())
+    } else if args[1].as_str() == "uppercase" {
+        println!("{}", input.to_uppercase())
+    } else if args[1].as_str() == "no-spaces" {
+        println!("{}", input.replace(" ", ""))
+    } else if args[1].as_str() == "slugify" {
+        println!("{}", slugify(input))
+    } else {
+        panic!("Invalid flag passed, `{}` isn't contained in -- `lowercase`,`uppercase`,`no-spaces`,`slugify`", args[1])
+    }
+}
