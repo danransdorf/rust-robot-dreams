15cc13da9b455c8bee169ee5ecda8b0e0e725e1b
MonteBro <czech.illusion@gmail.com>
2024-05-19 09:36:27 +0200

    feat: lesson6

--- lesson6/src/main.rs
+++ lesson6/src/main.rs
@@ -1,34 +1,53 @@
 use std::io::{ stdin, stdout, Write };
 use std::io::Read;
+use std::str::FromStr;
 use std::sync::mpsc::channel;
 use std::thread;
 
 mod operations;
 
-const VALID_OPERATIONS: [&str; 5] = ["lowercase", "uppercase", "no-spaces", "slugify", "csv"];
+enum Operation {
+    Lowercase,
+    Uppercase,
+    NoSpaces,
+    Slugify,
+    Csv,
+}
 
-fn detect_invalid_operation(operation: &str) -> bool {
-    if !VALID_OPERATIONS.contains(&operation) {
-        flush_print(
-            &format!(
-                "Operation `{}` isn't a possible operation. List of possible -- {}",
-                operation,
-                VALID_OPERATIONS.map(|string| format!("`{string}`")).join(",")
-            )
-        );
-        true
-    } else {
-        false
+#[derive(Debug)]
+enum OperationError {
+    InvalidOperation(String),
+}
+
+impl FromStr for Operation {
+    type Err = OperationError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.trim().to_lowercase().as_str() {
+            "lowercase" => Ok(Operation::Lowercase),
+            "uppercase" => Ok(Operation::Uppercase),
+            "no-spaces" => Ok(Operation::NoSpaces),
+            "slugify" => Ok(Operation::Slugify),
+            "csv" => Ok(Operation::Csv),
+            _ => Err(OperationError::InvalidOperation(s.to_string())),
+        }
     }
 }
 
+const VALID_OPERATIONS: [&str; 5] = ["lowercase", "uppercase", "no-spaces", "slugify", "csv"];
+
+fn flush_eprint(message: &str) {
+    eprintln!("{}", message);
+    stdout().flush().expect("Failed to flush stdout");
+}
 fn flush_print(message: &str) {
     println!("{}", message);
     stdout().flush().expect("Failed to flush stdout");
 }
 
 fn main() {
-    let (tx, rx) = channel::<String>();
+    let (operation_tx, operation_rx) = channel::<Operation>();
+    let (input_tx, input_rx) = channel::<String>();
     let (continue_tx, continue_rx) = channel::<()>();
 
     continue_tx.send(()).unwrap();
@@ -37,49 +56,48 @@ fn main() {
 
     let input_thread = thread::spawn(move || {
         while let Ok(_) = continue_rx.recv() {
-            let mut operation = String::new();
+            let mut input_buf = String::new();
 
-            flush_print("Operation:");
-            stdin()
-                .read_line(&mut operation)
-                .expect("Failed to read the input (line), that should represent an operation");
+            flush_print("Enter <operation>⎵<input> (Ctrl+D to submit):");
+            stdin().read_to_string(&mut input_buf).expect("Failed to read the input");
 
-            operation = operation.trim().to_string();
-
-            if detect_invalid_operation(&operation) {
+            if let Some((operation, input)) = input_buf.split_once(" ") {
+                match Operation::from_str(operation.trim()) {
+                    Ok(operation) => {
+                        operation_tx.send(operation).unwrap();
+                        input_tx.send(input.trim().to_string()).unwrap();
+                    }
+                    Err(OperationError::InvalidOperation(op)) => {
+                        flush_eprint(
+                            &format!(
+                                "Operation `{}` isn't a possible operation. List of possible -- {}",
+                                op,
+                                VALID_OPERATIONS.join(", ")
+                            )
+                        );
+                        continue_tx_clone.send(()).unwrap();
+                        continue;
+                    }
+                }
+            } else {
+                flush_eprint("No input provided, use format: <operation>⎵<input> \n");
                 continue_tx_clone.send(()).unwrap();
                 continue;
             }
-
-            let mut input = String::new();
-
-            flush_print("Input to process:");
-            if operation == "csv" {
-                flush_print("(Ctrl+D to end input)");
-                stdin().read_to_string(&mut input).expect("Failed to read the input (whole)");
-            } else {
-                stdin().read_line(&mut input).expect("Failed to read the input (line)");
-            }
-
-            input = input.trim().to_string();
-
-            tx.send(operation).unwrap();
-            tx.send(input).unwrap();
         }
     });
 
     let processing_thread = thread::spawn(move || {
         loop {
-            let operation = rx.recv().unwrap();
-            let input = rx.recv().unwrap();
+            let operation = operation_rx.recv().unwrap();
+            let input = input_rx.recv().unwrap();
 
-            let result = match operation.as_str() {
-                "lowercase" => operations::lowercase(input),
-                "uppercase" => operations::uppercase(input),
-                "no-spaces" => operations::no_spaces(input),
-                "slugify" => operations::slugify(input),
-                "csv" => operations::csv(input),
-                _ => unreachable!(),
+            let result = match operation {
+                Operation::Lowercase => operations::lowercase(input),
+                Operation::Uppercase => operations::uppercase(input),
+                Operation::NoSpaces => operations::no_spaces(input),
+                Operation::Slugify => operations::slugify(input),
+                Operation::Csv => operations::csv(input),
             };
 
             match result {
